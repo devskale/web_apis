5a40bf6f5f73e4db570752fb2af65d0e6504d840
devskale <dev@skale.io>
2024-08-29 23:20:20 +0200

    api

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ from duck.ducknews import search_news, search_text, search_maps, search_translat
 
 app = FastAPI()
 
-@app.get("/api/echo")
+@app.get("/echo")
 def echo(text: str = Query(default="Hello, World!", min_length=1)):
     cleaned_text = echoing(text)
     return {"echo": cleaned_text}
 
-@app.get("/api/w3m")
+@app.get("/w3m")
 def w3m_fetch(url: str):
     try:
         content = fetch_with_w3m(url)
@@ -20,28 +20,28 @@ def w3m_fetch(url: str):
         raise HTTPException(status_code=500, detail=str(e))
 
 # DuckDuckGo Search Routes
-@app.get("/api/duck/news")
+@app.get("/duck/news")
 def get_news(topic: str):
     results = search_news(topic)
     if not results:
         raise HTTPException(status_code=404, detail="No news found.")
     return {"results": results}
 
-@app.get("/api/duck/text")
+@app.get("/duck/text")
 def get_text(topic: str):
     results = search_text(topic)
     if not results:
         raise HTTPException(status_code=404, detail="No text found.")
     return {"results": results}
 
-@app.get("/api/duck/maps")
+@app.get("/duck/maps")
 def get_maps(topic: str, place: Optional[str] = None):
     results = search_maps(topic, place)
     if not results:
         raise HTTPException(status_code=404, detail="No maps found.")
     return {"results": results}
 
-@app.get("/api/duck/translate")
+@app.get("/duck/translate")
 def get_translation(topic: str, to_language: str):
     results = search_translate(topic, to_language)
     if not results:
